d4a1a1ba915a4872996bd76bb2f78b069252d149
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-10 19:58:19 -0300

    Subiendo proyecto Spring Boot booking-microservice

---
+++ src/test/java/com/ImplementandoDiscoveryService/booking_microservice/BookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ImplementandoDiscoveryService.booking_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
