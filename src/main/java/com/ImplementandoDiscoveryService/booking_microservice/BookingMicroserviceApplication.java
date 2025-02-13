9eb290887cedf80a081209ae4a3dd111b73953d5
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-13 00:58:55 -0300

    Moviendo cambios a Adaptar_Config_Server

--- src/main/java/com/ImplementandoDiscoveryService/booking_microservice/BookingMicroserviceApplication.java
+++ src/main/java/com/ImplementandoDiscoveryService/booking_microservice/BookingMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.ImplementandoDiscoveryService.booking_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BookingMicroserviceApplication {
 
