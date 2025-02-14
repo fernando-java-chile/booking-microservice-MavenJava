88114ac0f4a8b451a59de1ee5b2f87a96780df1c
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-13 21:51:28 -0300

    Refrescando las Configuraciones 1. Introducción a RefreshScope 2. Implementando RefreshScope 3. Probando RefreshScope en los Microservicios

---
+++ src/main/java/com/ImplementandoDiscoveryService/booking_microservice/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.ImplementandoDiscoveryService.booking_microservice.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/categories")
+@Getter
+@Setter
+@RefreshScope
+public class CategoryController {
+
+    @Value("${app.testProp}")
+    private String testProp;
+
+    // cambio del valor desde github a través del sistema config-server
+    // @RefreshScope: Refrescando las Configuraciones
+    // para ejecurtar los cambios en tiempo de ejecución
+    // POST a 192.168.100.14:8086/actuator/refresh
+    // y luego 192.168.100.14:8086/v1/categories/test
+    @RequestMapping("/test")
+    public String test() {
+        return testProp;
+    }
+
+}
